f96e96b231eba522a9c737f22e0a1850da25cf6b
Dhimant Patel <drp4kri@gmail.com>
2019-03-06 14:43:51 -0500

    The interpreter sets several variables before starting python program. One such variable is __name__. repeat: __name__ is a variable. Interpreter sets the value of the invoked file to "__main__". repeat: "__main__" is a value.
    Correct comparision is...
    if __name__ == "__main__":

--- HelloGithub.py
+++ HelloGithub.py
@@ -1,13 +1,11 @@
 import sys
 
-def sayHello():
-    """
-
-    :rtype: object
-    """
+def sayhello():
     print('Hello Github')
     return None
 
 
-if "__name__" == "__main__":
-    sayHello()
+if __name__ == "__main__":
+    sayhello()
+else:
+    print('__name__: ', __name__)
